fcc2e2d63b920ca7b581e931c4e604f3382c3da2
sayapakailinuxpak <sayapakailinuxpak@gmail.com>
2020-03-29 06:29:11 +0700

    adding forgot password feature

--- app/src/main/java/com/eldisprojectexpert/firebaseauthentication/MainActivity.java
+++ app/src/main/java/com/eldisprojectexpert/firebaseauthentication/MainActivity.java
@@ -1,8 +1,10 @@
 package com.eldisprojectexpert.firebaseauthentication;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,15 +13,18 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    TextView textViewSignUp;
+    TextView textViewSignUp, textViewForgotPassword;
     EditText editTextEmail, editTextPassword;
     Button buttonLogin;
     ProgressBar progressBarLogin;
@@ -42,6 +47,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         progressBarLogin = findViewById(R.id.progressbar_login);
         textViewSignUp.setOnClickListener(this);
         buttonLogin.setOnClickListener(this);
+        textViewForgotPassword = findViewById(R.id.textView5);
+        textViewForgotPassword.setOnClickListener(this);
     }
 
     @Override
@@ -81,6 +88,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                        }
                    }
                });
+
+            case R.id.textView5 :
+                final EditText editTextEmailForgotPassword = new EditText(v.getContext());
+                AlertDialog.Builder alertDialogForgotPassword = new AlertDialog.Builder(v.getContext());
+                alertDialogForgotPassword.setTitle("Reset Password ?");
+                alertDialogForgotPassword.setMessage("Enter Your Email to Receive Reset Link");
+                alertDialogForgotPassword.setView(editTextEmailForgotPassword);
+                alertDialogForgotPassword.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //extract the email and send the reset link
+                        final String emailToReset = editTextEmailForgotPassword.getText().toString();
+                        firebaseAuth.sendPasswordResetEmail(emailToReset).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(MainActivity.this, "Successfully reset password for " + emailToReset, Toast.LENGTH_SHORT).show();
+
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(MainActivity.this, "Failed to reset password because" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                Log.d("MainActivity", "onFailure : " + e.getMessage());
+                            }
+                        });
+                    }
+                });
+
+                alertDialogForgotPassword.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                //implement it
+                alertDialogForgotPassword.create().show();
 
         }
     }
